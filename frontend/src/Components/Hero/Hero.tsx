d944c0d421af91b9dacdb1bf41c431f3c402d858
Gio <beria.giorgi1@gmail.com>
2024-07-23 17:06:29 +0400

    add several navigation items

--- frontend/src/Components/Hero/Hero.tsx
+++ frontend/src/Components/Hero/Hero.tsx
@@ -11,11 +11,10 @@ const Hero = (props: Props) => {
       <div className="container flex flex-col-reverse mx-auto p-8 lg:flex-row">
         <div className="flex flex-col space-y-10 mb-44 m-10 lg:m-10 xl:m-20 lg:mt:16 lg:w-1/2 xl:mb-52">
           <h1 className="text-5xl font-bold text-center lg:text-6xl lg:max-w-md lg:text-left">
-            Financial data with no news.
+            Be better self and help others.
           </h1>
           <p className="text-2xl text-center text-gray-400 lg:max-w-md lg:text-left">
-            Search relevant financial documents without fear mongering and fake
-            news.
+            Follow our guidelines to achieve more and help others.
           </p>
           <div className="mx-auto lg:mx-0">
             <Link
@@ -27,7 +26,10 @@ const Hero = (props: Props) => {
           </div>
         </div>
         <div className="mb-24 mx-auto md:w-180 md:px-10 lg:mb-0 lg:w-1/2">
-          <img src={hero} alt="" />
+          <iframe width="600" height="560" src="https://www.youtube.com/embed/PE0u7-SX2hs?si=ks8_pv1Ki4UmQEgw"
+                  title="YouTube video player" frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
       </div>
     </section>
