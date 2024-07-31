912c3838480d8f4cb2fc9e0b2f46a798eafb2ab0
sudo-gnp <gnoergaardp@hotmail.com>
2024-07-31 08:59:11 +0200

    addsd

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01c1221042e08bda2cc5fe02860f80822dd2f6ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+697afed9a13872cf74b9af979d0d578bb4b69c01")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
